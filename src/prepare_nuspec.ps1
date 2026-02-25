bf1f2847db11035bd43cd566b123229e9ddd6f20
Michael Clayton <mike@kingslandconsulting.co.uk>
2026-02-25 21:08:13 +0000

    build nupkg

---
+++ src/prepare_nuspec.ps1
@@ -0,0 +1,37 @@
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version "Latest"
+
+# install the gitversion tool if it isn't already installed
+# and get the version numbers for this commit
+$tools      = dotnet tool list --format json | convertfrom-json
+$gitversion = $tools.data | where-object { $_.packageId -eq "gitversion.tool" }
+if ($null -eq $gitVersion)
+{
+	dotnet tool install GitVersion.Tool --version 5.8.2
+}
+$versions = dotnet-gitversion | convertfrom-json
+write-host "versions = "
+write-host ($versions | format-list | out-string)
+
+# get the target framework from the csproj
+$csproj = [xml] (get-content "./src/Kingsland.MofParser/Kingsland.MofParser.csproj" -raw)
+$targetFramework = $csproj.Project.PropertyGroup.TargetFramework | where-object { $null -ne $_ }
+write-host "target framework = '$targetFramework'"
+
+# insert values into the nuspec file
+$nuspecPath = "./.build/Kingsland.MofParser.nuspec"
+$nuspecText = get-content $nuspecPath -raw
+write-host "nuspec before = "
+write-host "------------"
+$nuspecText
+write-host "------------"
+$nuspecText = $nuspecText.Replace("{{ commit }}", $versions.Sha)
+$nuspecText = $nuspecText.Replace("{{ version }}", $versions.NuGetVersionV2)
+$nuspecText = $nuspecText.Replace("{{ targetFramework }}", $targetFramework)
+write-host "nuspec after = "
+write-host "------------"
+$nuspecText
+write-host "------------"
+
+# write the versioned nuspec
+set-content -Path $nuspecPath -Value $nuspecText
